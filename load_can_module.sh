7d0f0ff6d55b46198482babfce3b72f25c6f83fe
Donghee Yi <dhyi@hyunbo.com>
2025-02-04 10:43:12 +0900

    Error handling parts added to module loading script.

--- load_can_module.sh
+++ load_can_module.sh
@@ -1,13 +1,66 @@
 #!/bin/bash
 
+CAN_LOADED=0
+VCAN_LOADED=0
+
 echo ""
+echo "[CAN Modules]"
 echo Loading CAN module...
 sudo modprobe can
+
+echo Loading VCAN module...
+sudo modprobe vcan
+
+if [ $? -eq 0 ]; then
+    VCAN_LOADED=1
+else
+    echo "Loading failed. Stopped."
+    exit 1
+fi
+
+if [ $? -eq 0 ]; then
+    CAN_LOADED=1
+else
+    echo "Loading failed. Stopped."
+    exit 1
+fi
+
 echo Loading ISO-TP module...
 sudo modprobe can_isotp
-echo Loading PCAN-USB module...
-sudo modprobe peak_usb
-echo Complete.
+
+if [ $? -eq 0 ]; then
+    echo Loading PCAN-USB module...
+    sudo modprobe peak_usb
+
+    if [ $? -eq 0 ]; then
+        echo Complete.
+    else
+        # 명령어 실패시
+        echo "Failed. Loading skipped."
+    fi
+else
+    echo "Loading failed. Stopped."
+    exit 1
+fi
+
 echo ""
 lsmod | grep can
+echo ""
+
+echo "[CAN Interface]"
+if [ $VCAN_LOADED == 1 ]; then
+    echo Adding VCAN interface...
+    sudo ip link add dev vcan0 type vcan
+else
+    echo VCAN interface init skipped.
+fi
+
+if [ $CAN_LOADED == 1 ]; then
+    echo Adding CAN interface...
+    sudo ip link add dev can0 type can
+else
+    echo CAN interface init skipped.
+fi
+
+echo Complete.
 echo ""
